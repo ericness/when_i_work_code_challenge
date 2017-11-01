afc144e037e614ac31bf154cabb081e621bef6f2
Ubuntu <ericness@UdemySparkCourse.fpcodpc5vfjurkxi5gs5htsn0d.gx.internal.cloudapp.net>
2017-11-01 17:04:14 +0000

    Added function to clean data in incoming data frames

--- transform_web_traffic.py
+++ transform_web_traffic.py
@@ -27,6 +27,30 @@ if args.output:
 else:
     OUTPUT_CSV_NAME='web_traffic.csv'
 
+def clean_web_traffic_data(web_traffic_df, s3_object_name):
+
+    frame_size = len(web_traffic_df.index)
+
+    # check that format of path is valid. remove any invalid rows.
+    web_traffic_df = web_traffic_df[web_traffic_df['path'].str.match('^(/\w*)+')==True]
+    
+    filter_count = frame_size - len(web_traffic_df.index)
+    
+    if filter_count != 0:
+        print(f'{filter_count} rows filtered out of {s3_object_name} because of invalid path formats.')
+        frame_size = len(web_traffic_df.index)
+    
+    # check that all length values are integers
+    if web_traffic_df['length'].dtype != 'int64':
+        web_traffic_df = web_traffic_df[web_traffic_df['length'].to_string().str.isdigit()==True]
+ 
+        filter_count = frame_size - len(web_traffic_df.index)
+    
+        if filter_count != 0:
+            print(f'{filter_count} rows filtered out of {s3_object_name} because of non-integer length.')
+        
+        web_traffic_df['length'] = web_traffic_df['length'].astype(int)
+    
 # use the UNSIGNED signature version for anonymous access
 s3 = boto3.resource('s3', config=botocore.client.Config(signature_version=botocore.UNSIGNED))
 
@@ -51,13 +75,13 @@ try:
             obj = s3.Object(s3_obj.bucket_name, s3_obj.key)
             web_traffic_subset = pd.read_csv(io.BytesIO(obj.get()['Body'].read()), encoding='utf8')
  
-            # check structure and contents of dataframe
-            
+            # check structure and contents of dataframe            
             if set(['user_id','path','length']).issubset(web_traffic_subset.columns):
-                
-                
+                clean_web_traffic_data(web_traffic_subset, s3_obj.key)
                 
                 web_traffic_list.append(web_traffic_subset[['user_id','path','length']])
+            else:
+                print(f'Data in file {s3_obj.key} was skipped because it does not contain fields user_id, path and length')
                 
 except botocore.exceptions.ClientError as e:
     print(e.response['Error']['Message'])
@@ -74,5 +98,8 @@ web_traffic_user = web_traffic_user_path.reset_index()
 web_traffic_user['length'] = web_traffic_user['length'].astype('int')
 web_traffic_user = web_traffic_user.pivot(index='user_id',columns='path',values='length')
 web_traffic_user = web_traffic_user.fillna(0)
+
+# dtype converts to float when pivoting because of the presence of NaNs.
+# convert the data type back to int.
 web_traffic_user = web_traffic_user.astype(dtype='int')
 web_traffic_user.to_csv(OUTPUT_CSV_NAME)
